53a18f541925c803d11ed2f5301ae86f23d6a6f1
Andre Aguirre <andreaguirre007@gmail.com>
2025-09-13 23:20:32 -0600

    wsda

--- src/practica/sistemas/numericos/PracticaSistemasNumericos.java
+++ src/practica/sistemas/numericos/PracticaSistemasNumericos.java
@@ -1,26 +1,27 @@
-package practica.sistemas.numericos;
+package practica.sistemas.numericos; // paquete smn
 
-import javax.swing.JOptionPane;
+import javax.swing.JOptionPane; // importar coso para q se vea todo mas bonito
 
-public class PracticaSistemasNumericos {
+public class PracticaSistemasNumericos { // hacer clase PracticaSistemasNumericos ahi
 
-    public static String convertirDecimalAOctal(int dec) {
-        if (dec == 0) return "0";  // Caso especial
+    public static String convertirDecimalAOctal(int dec) { // le metes como param un decimal
+        if (dec == 0) return "0";  // Este coso si el decimal es 0 obvio el octal tmb
         
-        String octal = "";
-        while (dec > 0) {
-            int residuo = dec % 8;
-            octal = residuo + octal;  // Esto está bien
-            dec = dec / 8;
+        String octal = ""; // iniciar una variable sin nadota
+        while (dec > 0) { // iniciar un bucle pa que se repita hasta q sea menor a 0
+            int residuo = dec % 8; // guardar el residuo de dividir / 8
+            octal = residuo + octal;   // guardar cada digito en la variable octal
+            dec = dec / 8; // el decimal de va dividiendo por 8 yes yes
         }
-        return octal;
+        return octal; // devolver el resultado en octal
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { // metodo main, pa que todo jale
         int decimal = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero en decimal: "));
+        // Le preguntas al user, pon un decimal aja:
         
        
-        String resultado = convertirDecimalAOctal(decimal);
-        JOptionPane.showMessageDialog(null, decimal + " en octal es: " + resultado);
+        String resultado = convertirDecimalAOctal(decimal); // usas la funcion q hicimos antes y se guarda en resutltado
+        JOptionPane.showMessageDialog(null, decimal + " en octal es: " + resultado); // Muestra q si es el resultado
     }
 }
